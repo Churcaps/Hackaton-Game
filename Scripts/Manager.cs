b79ef677f013ba378332dfb3168ec3069a91da92
Paccapelo Auguste <a.paccapelo@student.isartdigital.com>
2025-06-09 12:10:20 +0200

    Added manager script

---
+++ Scripts/Manager.cs
@@ -0,0 +1,91 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+// Author : PACCAPELO Auguste
+
+namespace Com.IsartDigital.Hackaton
+{
+	public abstract class Manager : Node2D
+	{
+		// ---------- VARIABLES ---------- \\
+
+		// ----- Managers ----- \\
+		public static int numManager { get; private set; } = 0;
+		private static Dictionary<Type, Manager> allManagers = new Dictionary<Type, Manager>();
+		public static Action allManagersFinishedInits;
+		public static bool isInitsFinished { get; private set; } = false;
+		public Type thisType { get; private set; } = null;
+
+		// ---------- FUNCTIONS ---------- \\
+
+		// ----- Constructor & Start & Init ----- \\
+
+		protected Manager() : base()
+		{
+			// Count how many Managers instances have been created
+			numManager++;
+		}
+
+		public override void _Ready()
+		{
+			thisType = GetType();
+
+			// Prevent duplicate manager of the same type
+			if (allManagers.ContainsKey(thisType))
+			{
+				GD.Print("This manager : " + thisType + " already exist, destroying last added.");
+				numManager--;
+				Destructor();
+				return;
+			}
+
+			allManagers.Add(thisType, this);
+			if (IsAllManagersReady()) InitAllManagers();
+		}
+
+		protected abstract void Init();
+
+		// ----- My Functions ----- \\
+
+		/// <summary>
+		/// Retrieves the instance of the specified manager type, if it exist.
+		/// </summary>
+		/// <typeparam name="T">The manager type to retrieve</typeparam>
+		/// <returns>The manager instance, or null if not found</returns>
+		public static T GetManager<T>() where T : Manager
+		{
+			Type lManagerType = typeof(T);
+			if (allManagers.ContainsKey(lManagerType))
+				return (T)allManagers[lManagerType];
+
+			GD.Print("Manager of type : " + lManagerType + " not found.");
+			return null;
+		}
+
+		private bool IsAllManagersReady() => numManager == allManagers.Count;
+		private void InitAllManagers()
+		{
+			if (!isInitsFinished)
+			{
+				foreach (Manager lManager in allManagers.Values) lManager.Init();
+				isInitsFinished = true;
+				allManagersFinishedInits?.Invoke();
+				return;
+			}
+			Init();
+		}
+
+		// ----- Destructor ----- \\
+
+		public virtual void Destructor()
+		{
+			if (allManagers[thisType] == this)
+			{
+				numManager--;
+				allManagers.Remove(thisType);
+			}
+			QueueFree();
+		}
+	}
+}
