93de75282840b874c4fc55623f6d54974d253459
Arnaud <a.partakelidis@student.isartdigital.com>
2025-06-10 10:35:23 +0200

    Adding Character Selection + Phone & Book Button

---
+++ Scripts/UI/CharacterButton.cs
@@ -0,0 +1,77 @@
+using Godot;
+using System;
+
+// Author : Arnaud Partakelidis
+
+namespace Com.IsartDigital.ProjectName
+{
+	public partial class CharacterButton : TextureButton
+	{
+		public Vector2 characterPos;
+
+		private bool currentlyAnimateButtonPressed;
+		private Timer characterSelectTimer;
+		
+		public override void _Ready()
+		{
+			characterPos = Position;
+
+			Pressed += PlayButtonPress;
+
+			characterSelectTimer = new Timer();
+			characterSelectTimer.WaitTime = 1f;
+			characterSelectTimer.OneShot = true;
+			AddChild(characterSelectTimer);
+
+			characterSelectTimer.Timeout += () => currentlyAnimateButtonPressed = false;
+		}
+
+		public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+
+		}
+
+		private void PlayButtonPress()
+		{
+			if (!currentlyAnimateButtonPressed)
+			{
+				GetParent<CharacterSelection>().skipButton.Visible = true;
+				currentlyAnimateButtonPressed = true;
+
+				for (int i = 0; i < GetParent<CharacterSelection>().charactersButton.Count; i++)
+				{
+					if (GetParent<CharacterSelection>().charactersButton[i] == this)
+					{
+						Tween lTween = GetTree().CreateTween().SetTrans(Tween.TransitionType.Quad).SetEase(Tween.EaseType.Out).SetParallel();
+
+						lTween.TweenProperty(this, "scale", Vector2.One * 1.1f, 0.5f);
+						lTween.TweenProperty(this, "position", Vector2.Zero, 0.5f);
+
+						GetParent<CharacterSelection>().canShowText = true;
+						GetParent<CharacterSelection>().characterInfo.Visible = true;
+
+						continue;
+					}
+
+					Tween lTween2 = GetTree().CreateTween().SetTrans(Tween.TransitionType.Quad).SetEase(Tween.EaseType.Out).SetParallel();
+					lTween2.TweenProperty(GetParent<CharacterSelection>().charactersButton[i], "scale", Vector2.Zero, 0.2f);
+
+					GetParent<CharacterSelection>().closeButton.Visible = true;
+					GetParent<CharacterSelection>().playButton.Visible = true;
+					lTween2.TweenProperty(GetParent<CharacterSelection>().closeButton, "modulate", Colors.White, 0.5f);
+					lTween2.TweenProperty(GetParent<CharacterSelection>().playButton, "modulate", Colors.White, 0.5f);
+					lTween2.TweenProperty(GetParent<CharacterSelection>().playButton, "position", GetParent<CharacterSelection>().spawnPlayPos, 0.5f);
+				}
+
+				characterSelectTimer.Start();
+			}
+		}
+			
+	}
+}
---
+++ Scripts/UI/InGame.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class InGame : Control
+{
+	[Export] private TextureButton book, phone;
+	[Export] private ColorRect livreImage, phoneImage;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		book.Pressed += OpenBook;
+		phone.Pressed += OpenPhone;
+		livreImage.GetChild<Button>(0).Pressed += CloseBook;
+		phoneImage.GetChild<Button>(0).Pressed += ClosePhone;
+
+		Tween lTween = GetTree().CreateTween().SetTrans(Tween.TransitionType.Quart).SetEase(Tween.EaseType.Out);
+		lTween.TweenProperty(this, "modulate", Colors.White, 0.8f);
+
+	}
+
+	private void CloseBook()
+	{
+		livreImage.GetChild<Button>(0).Visible = false;
+		Tween lTween = GetTree().CreateTween().SetTrans(Tween.TransitionType.Quart).SetEase(Tween.EaseType.Out);
+		lTween.TweenProperty(livreImage, "modulate", Colors.Transparent, 0.5f);
+	}
+
+	private void OpenBook()
+	{
+		livreImage.GetChild<Button>(0).Visible = true;
+		Tween lTween = GetTree().CreateTween().SetTrans(Tween.TransitionType.Quart).SetEase(Tween.EaseType.Out);
+		lTween.TweenProperty(livreImage, "modulate", Colors.White, 0.5f);
+	}
+
+	private void ClosePhone()
+	{
+		phoneImage.GetChild<Button>(0).Visible = false;
+		Tween lTween = GetTree().CreateTween().SetTrans(Tween.TransitionType.Quart).SetEase(Tween.EaseType.Out);
+		lTween.TweenProperty(phoneImage, "modulate", Colors.Transparent, 0.5f);
+	}
+
+	private void OpenPhone()
+	{
+		phoneImage.GetChild<Button>(0).Visible = true;
+		Tween lTween = GetTree().CreateTween().SetTrans(Tween.TransitionType.Quart).SetEase(Tween.EaseType.Out);
+		lTween.TweenProperty(phoneImage, "modulate", Colors.White, 0.5f);
+	}
+}
